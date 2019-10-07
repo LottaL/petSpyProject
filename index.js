5372464288c1b6969232c0086baf479ebb323d9f
mikaup <muponen@gmail.com>
2019-10-07 13:41:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 "use strict";
 //sample how to use modules/fetching.js functions
 
-getAllStreams().then(res => console.log(res))
+getAllStreams().then(res => console.log(res));
+
+const registerbutton = document.getElementById("nappi");
+
+const loginbutton = document.getElementById("loginnappi");
+
+const username = document.getElementById("registerusername");
+
+const email =  document.getElementById("registeremail");
+
+const password = document.getElementById("registerpassword");
+
+registerbutton.addEventListener("click", function(event){
+  event.preventDefault();
+  console.log(event);
+    registerUser(username,password,email)
+});
+
+
+
+loginbutton.addEventListener("click", function(event){
+event.preventDefault();
+console.log(event);
+
+});
